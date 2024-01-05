b4349562a05369cabc770e4165494c3c08be98e9
1Himanshusharma <himanshusharma272002@gmail.com>
2024-01-05 12:24:01 +0530

    js

---
+++ basic/comparison.js
@@ -0,0 +1,9 @@
+let a = 5;
+let b = 6;
+let c = "5";
+console.log("a==b: ",a==b);
+console.log("a!=b: ",a!=b);
+console.log("a===b: ",a===b);
+console.log("a!==b: ",a!==b);
+
+console.log("a == c:",a==c);
