37ddd2ab9bf1209c716ef8956d1a85308f84d3d5
1Himanshusharma <himanshusharma272002@gmail.com>
2024-01-05 11:19:29 +0530

    js

---
+++ basic/variables.js
@@ -0,0 +1,17 @@
+// var that can be redefined
+// let that can not redefined but update
+// const that be update
+var FullName = "himanshu Sharma";
+console.log(FullName);
+var FullName = "sharma";
+console.log(FullName);
+
+let Name = "Himanshu Sharma";
+console.log(Name);
+Name = "Sharma";
+console.log(Name);
+
+
+const MyName = "Himanshu Sharma";
+console.log(MyName);
+console.log(MyName);
