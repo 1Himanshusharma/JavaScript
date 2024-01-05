382e00d261e9cedfe109d5ab591d4c5403636b67
1Himanshusharma <himanshusharma272002@gmail.com>
2024-01-05 14:32:40 +0530

    js

---
+++ basic/conditional.js
@@ -0,0 +1,21 @@
+//conditional statement
+let age = 25;
+
+if (age > 18){
+  console.log("you can vote.");
+}
+age = 15;
+if (age < 18){
+  console.log("you can not");
+}
+else{
+  console.log("you can not");
+}
+
+let num = 30
+if (num % 2 == 0){
+  console.log("This is even.");
+}
+else{
+  console.log("This is odd.");
+}
---
+++ basic/ternairy.js
@@ -0,0 +1,8 @@
+let age = 25;
+age >= 18 ? console.log("YOU are legel.") : console.log("You are not allowed");
+
+
+let num = 30;
+(num % 2 == 0) ? console.log("the nume is even.") : console.log("the num is odd.");
+
+
